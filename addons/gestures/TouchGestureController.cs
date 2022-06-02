61a0f9c4dd6074e2061897e27a5cc83c63f371a9
Matthijs <matthijsmud@gmail.com>
2022-06-02 17:04:57 +0200

    Class responsible for parsing touch gestures.

---
+++ addons/gestures/TouchGestureController.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Disposables;
+
+namespace Godot.Gestures
+{
+  sealed class TouchGestureController : Node
+  {
+
+  }
+}
